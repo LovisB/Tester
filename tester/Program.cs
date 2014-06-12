d0f4dd02190d80971ecef903c4ab50f765110381
Lovis <lovis@dmacmedia.ie>
2014-06-12 14:32:52 +0100

    My First Commit to the git Hub Universe

---
+++ tester/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Today);
+//
+            bool isComplex = Regex.IsMatch("Mmssssasdasdasdasdasdsadsada45'", @"^.{6,16}(?<=\d.*)(?<=[^a-zA-Z0-9].*)(?<=[A-Z].*)(?<=[A-Z].*)$");
+            Console.WriteLine(isComplex);
+        }
+    }
+}
